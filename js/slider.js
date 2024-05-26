a7b9ddc0bbd30ed3bbe9a4e2b4a120e869b33857
Kir2023 <kirill.kaydanovskyi@gmail.com>
2024-05-26 17:16:54 +0300

    optimaze structure

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,12 @@
-document.querySelector('.controls').addEventListener('click', event => {
-  const $slide = document.querySelector(event.target.getAttribute('href'));
-  if (!$slide) return;
+// document.querySelector('.controls').addEventListener('click', event => {
+//   const $slide = document.querySelector(event.target.getAttribute('href'));
+//   if (!$slide) return;
 
-  if ($slide.scrollIntoViewIfNeeded) {
-    event.preventDefault();
-    $slide.scrollIntoViewIfNeeded();
-  } else if ($slide.scrollIntoView) {
-    event.preventDefault();
-    $slide.scrollIntoView();
-  }
-});
+//   if ($slide.scrollIntoViewIfNeeded) {
+//     event.preventDefault();
+//     $slide.scrollIntoViewIfNeeded();
+//   } else if ($slide.scrollIntoView) {
+//     event.preventDefault();
+//     $slide.scrollIntoView();
+//   }
+// });
